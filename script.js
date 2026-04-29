7d4ec24c1f8fee41a005cce557417d9decdf8f0c
Eduardo Antonio de Souza <969658@senacminas.edu.br>
2026-04-29 10:38:22 -0300

    new function

--- script.js
+++ script.js
@@ -3,3 +3,15 @@ function Clicou() {
     var dataString = dataAtual.toLocaleString();
     document.getElementById("hora-atual").innerText = "hora atual : " + dataString;
 };
+
+function mudarCor() {
+    const cores = [
+        "linear-gradient(135deg, #ff9a9e, #fad0c4)",
+        "linear-gradient(135deg, #a18cd1, #fbc2eb)",
+        "linear-gradient(135deg, #f6d365, #fda085)",
+        "linear-gradient(135deg, #84fab0, #8fd3f4)"
+    ];
+
+    const corAleatoria = cores[Math.floor(Math.random() * cores.length)];
+    document.body.style.background = corAleatoria;
+}
